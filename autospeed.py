8e5d210df621b0b730267017fc9b7d76bc26609a
Eriós VHR <erios.inf@gmail.com>
2024-06-01 16:23:23 +0200

    Add files via upload
    Modified files for rock5C

---
+++ autospeed.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import sync_playwright
+
+def run(playwright):
+    browser = playwright.chromium.launch(
+        headless=False,
+        args =['--ignore-certificate-errors'],
+    )
+    context = browser.new_context()
+    page = context.new_page()
+
+    try:
+        page.goto('https://movispeed.es/movispeed/', wait_until="domcontentloaded")
+    except playwright._impl._errors.TimeoutError:
+        pass
+    page.evaluate('iniciarTest()')
+
+    limit_loops=30
+
+    for i in range(limit_loops):
+        print(f"Esperant...{i}/{limit_loops}")
+
+        page.wait_for_load_state("networkidle")
+        page.wait_for_timeout(1000)
+
+        total_download_mbps=page.locator('#textoIzquierda').inner_html()
+        total_upload_mbps=page.locator('#textoDerecha').inner_html()
+        
+        if float(total_download_mbps) > 0 and float (total_upload_mbps)>0:
+            break
+    else:
+        print ("No s'ha pogut obtindre la velocitat.")
+        return False, None, None
+
+    context.close()
+    browser.close()
+
+    return True, total_download_mbps,total_upload_mbps
+
+with sync_playwright() as playwright:
+    success, total_download_mbps,total_upload_mbps = run (playwright)
+
+    print("success: ", success)
+    print("Download: ", total_download_mbps," Mbps")
+    print("Upload ", total_upload_mbps," Mbps")
---
+++ script_movispeed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+#Activa entorno virtual
+source "/home/nenter/venv/bin/activate"
+#ejecuta python3 desde entorno virtual
+python3 /home/nenter/Desktop/autospeed.py
