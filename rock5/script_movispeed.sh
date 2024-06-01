c7c24f355cbda39daf3e405b43ad7546dc0b6731
Eriós VHR <erios.inf@gmail.com>
2024-06-01 16:31:28 +0200

    Crontab añadido

--- rock5/script_movispeed.sh
+++ rock5/script_movispeed.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-
+#crontab -l
+# añadir ultimas lineas de https://github.com/Nenter/movispeed/blob/main/rock5/crontab
+#
+#@reboot /home/nenter/script_movispeed.sh
+#* * * * * /home/nenter/script_movispeed.sh
+#
 #Activa entorno virtual
 source "/home/nenter/venv/bin/activate"
 #ejecuta python3 desde entorno virtual
