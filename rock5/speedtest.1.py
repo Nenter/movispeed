56fa9106ab6cc2cf4cc0ed6b0e56f229ec06f2ea
Eriós VHR <erios.inf@gmail.com>
2024-06-05 23:19:23 +0200

    Update speedtest.1.py

--- rock5/speedtest.1.py
+++ rock5/speedtest.1.py
@@ -1,6 +1,10 @@
 import speedtest
+import subprocess
 
 st = speedtest.Speedtest()
 
+subprocess.call("scriptInicio.sh")
 print("Download: %s Mbps" % (st.download() / 1024 / 1024))
+
 print("Upload: %s Mbps" % (st.upload() / 1024 / 1024))
+subprocess.call("scriptFin.sh")
