880169f8607937eda1a13b8ffe45fd23d1f7af06
Eriós VHR <erios.inf@gmail.com>
2024-06-24 22:51:10 +0200

    Update script_movispeed.sh

--- rock5/script_movispeed.sh
+++ rock5/script_movispeed.sh
@@ -8,7 +8,4 @@
 #
 #gpiodlib gpioset gpiochipX GPIOPIN=1; 1:HIGH, 0:LOW
 
-#Activa entorno virtual
-source /home/nenter/venv/bin/activate
-#ejecuta python3 desde entorno virtual
-python3 /home/nenter/Desktop/autospeed.py
+python3 autospeed.py
